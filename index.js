77845cb39f70e036dea3318d418c6784ccf57d25
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-03-27 19:42:08 -0700

    Writes cards and counts down

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Jess", "Ava", "Azeezah", "Ron", "Herm"];
+const messages = []
+
+function writeCards(names, eventName){
+    for (let i=0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+writeCards(names, eventName);
+
+function countDown(number){
+    let i = number;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
